603b24df577a152f944305d5dff82d4bd5add7f4
brett <brettlorsch@gmail.com>
2018-01-31 14:50:39 -0700

    lorscheider tool initial commit

---
+++ lib/Lorscheider_view_tool.rb
@@ -0,0 +1,6 @@
+require "Lorscheider_view_tool/version"
+require "Lorscheider_view_tool/version/renderer"
+
+module LorscheiderViewTool
+  # Your code goes here...
+end
---
+++ lib/Lorscheider_view_tool/version.rb
@@ -0,0 +1,3 @@
+module LorscheiderViewTool
+  VERSION = "0.1.0"
+end
